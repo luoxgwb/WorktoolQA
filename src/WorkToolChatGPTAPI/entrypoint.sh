5674d744926f311dd7f9480a099d5bc0ed762d73
luoxgwb <83362632@qq.com>
2023-04-10 10:32:50 +0800

    修改端口

--- src/WorkToolChatGPTAPI/entrypoint.sh
+++ src/WorkToolChatGPTAPI/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 注意: $PORT 非常重要, Railway 必须监听此端口
-export ASPNETCORE_URLS="http://+:5033"
+export ASPNETCORE_URLS="http://+:80"
 export ASPNETCORE_ENVIRONMENT="Production"
 export TZ="Asia/Shanghai"
 
